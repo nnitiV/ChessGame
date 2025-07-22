0f3f65dba6565cc6235e5e0fb0c9c9f403247568
nnitivv <vihstudent@gmail.com>
2025-07-21 21:10:29 -0300

    Added the remaining pawns I forgot haha

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -6,12 +6,12 @@ import Promote from "./Promote";
 const Board = () => {
     const intialBoardState = [
         ["br", "bn", "bb", "bq", "bk", "bb", "bn", "br"],
-        ["bp", "bp", "bp", "bp", "", "bp", "bp", "bp"],
+        ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
         ["", "", "", "", "", "", "", ""],
         ["", "", "", "", "", "", "", ""],
         ["", "", "", "", "", "", "", ""],
         ["", "", "", "", "", "", "", ""],
-        ["wp", "wp", "wp", "wp", "", "wp", "wp", "wp"],
+        ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
         ["wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"]
     ];
     // const intialBoardState = [
